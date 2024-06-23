ad76365a734153cba7b0d7423ef043941ef920c5
KaabaliXBoss <123297701+MrSoftyXTrillion@users.noreply.github.com>
2024-06-24 01:22:44 +0600

    Update P.Count Words.c

--- P.Count Words.c
+++ P.Count Words.c
@@ -1,17 +1,25 @@
-#include<stdio.h>
-#include<string.h>
-int main ()
-{
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
     char s[1000001];
-    int i,count=0;
-    gets(s);
-    for (i=0;i<strlen(s);i++)
-    {
-        if (s[i]==' ')
-        {
-            count++;
+    fgets(s, sizeof(s), stdin);  // Use fgets to read the input safely
+
+    int i, len = strlen(s), count = 0;
+    int inWord = 0;
+
+    for (i = 0; i < len; i++) {
+        if (isalpha(s[i])) {
+            if (!inWord) {
+                count++;
+                inWord = 1;
+            }
+        } else {
+            inWord = 0;
         }
     }
-    printf ("%d\n",count+1);
+
+    printf("%d\n", count);
     return 0;
 }
